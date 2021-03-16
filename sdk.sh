f55bd319f07844cda963a8e848fc9ac495d8b95a
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-17 05:24:22 +0800

    Update sdk.sh

--- sdk.sh
+++ sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 DL_NAME="openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz"
-curl -O -L https://github.com/qpzr/low/releases/download/v5.4/sdk.tar.xz
+curl -O -L https://github.com/qpzr/low/releases/download/v5.4/$DL_NAME
 mkdir -p sdk
 tar -xJf $DL_NAME -C sdk
 cp -rf sdk/*/staging_dir/* ./staging_dir/
