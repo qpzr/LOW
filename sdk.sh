4df683187d8a7825ac88200cf5cba900836dcb7f
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-26 04:16:41 -0500

    Update sdk.sh

--- sdk.sh
+++ sdk.sh
@@ -4,6 +4,7 @@ DL_NAME="openwrt-sdk-ipq40xx-generic_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz"
 curl -O -L https://github.com/qpzr/low/releases/download/v5.4/$DL_NAME
 mkdir -p sdk
 tar -xJf $DL_NAME -C sdk
+mv -f sdk/*/build_dir ./build_dir
 cp -rf sdk/*/staging_dir/* ./staging_dir/
 rm -rf $DL_NAME sdk
 sed -i '/\(target\|tools\|toolchain\)\/Makefile/d' Makefile
