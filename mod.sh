420b42ef482a4433712eb9409d0bdb59b594eb8c
qpzr <58745620+qpzr@users.noreply.github.com>
2021-07-02 09:40:57 -0500

    Create mod.sh

--- mod.sh
+++ mod.sh
@@ -8,7 +8,7 @@ sed -i 's/192.168.1.1/192.168.7.1/g' package/base-files/files/bin/config_generat
  
 echo '修改时区NTP'
 #sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
-sed -i 's/ntp1.aliyun.com/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
+sed -i 's/ntp.aliyun.com/cn.ntp.org.cn/g' package/base-files/files/bin/config_generate
 sed -i 's/time1.cloud.tencent.com/ntp.ntsc.ac.cn/g' package/base-files/files/bin/config_generate
 sed -i '/time.ustc.edu.cn/d' package/base-files/files/bin/config_generate
 sed -i '/cn.pool.ntp.org/d' package/base-files/files/bin/config_generate
