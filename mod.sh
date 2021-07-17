320494d94e6dab22354dce255dce51534d45dd94
qpzr <58745620+qpzr@users.noreply.github.com>
2021-07-17 11:11:53 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -32,7 +32,7 @@ echo '去吧皮卡丘'
 cd package
  
 echo 'luci-app-AdGuardHome'
-svn export https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
  
 echo '最新argon主题和设置'
 rm -rf ./lean/luci-theme-argon
