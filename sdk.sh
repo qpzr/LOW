e3b411b744db6dbb7a2064a6bc4de7ad2b75141a
qpzr <58745620+qpzr@users.noreply.github.com>
2022-10-19 04:57:52 +0800

    Update sdk.sh

--- sdk.sh
+++ sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-DL_NAME="openwrt-sdk-ipq40xx_gcc-8.4.0_musl_eabi.Linux-x86_64.tar.xz"
+DL_NAME="openwrt-sdk-ipq40xx_gcc-11.3.0_musl_eabi.Linux-x86_64.tar.xz"
 curl -O -L https://github.com/qpzr/low/releases/download/v5.4/$DL_NAME
 mkdir -p sdk
 tar -xJf $DL_NAME -C sdk
