d5d3e9019dde59af203c9b32b7145c0759483c5c
qpzr <58745620+qpzr@users.noreply.github.com>
2026-09-05 04:39:22 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -12,9 +12,9 @@ sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/theme
 echo '修改wifi名称'
 sed -i 's/LEDE/G-DOCK/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-#echo 'Turboacc设置'
-#sed -i 's/+xz//g' feeds/luci/applications/luci-app-adguardhome/Makefile
-#sed -i 's/+xz-utils//g' feeds/luci/applications/luci-app-adguardhome/Makefile
+echo 'Turboacc设置'
+sed -i 's/5.10/6.18/g' target/linux/ipq40xx/Makefile
+sed -i 's/LUCI_EXTRA_DEPENDS/d' feeds/luci/applications/luci-app-adguardhome/Makefile
 
 echo '去吧皮卡丘'
 cd package
